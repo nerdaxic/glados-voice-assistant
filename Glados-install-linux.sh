6fd59c5d0b539f417c42a7c6f25188c7512b6413
12tae12 <84397906+12tae12@users.noreply.github.com>
2023-02-04 17:06:54 -0500

    Change install file

--- Glados-install-linux.sh
+++ Glados-install-linux.sh
@@ -6,10 +6,11 @@ sudo apt install portaudio19-dev
 sudo apt-get install -y build-essential swig libpulse-dev libasound2-dev
 sudo apt-get install flac
 git clone --recurse-submodules https://github.com/nerdaxic/glados-voice-assistant/
-cd ~/glados-voice-assistant
-sudo pip3 install -r requirements.txt
+sudo useradd 12tae12
+cd ~/home/12tae12/glados-voice-assistant
+sudo 12tae12 pip3 install -r requirements.txt
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-sudo pip3 install -r requirements.txt
+sudo 12tae12 pip3 install -r requirements.txt
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 cd ~/glados-voice-assistant/glados_tts/
 python3 glados.py
